11819060ecd03f290d574a3bea735cc1f2854262
Saqlain Riaz <saqlainprinters@gmail.com>
2024-04-02 13:39:30 +0500

    feat: deployments setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,62 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-ignition-viem";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
+const mnemonic: string | undefined = process.env.MNEMONIC;
+const accounts = { mnemonic };
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "TTJKDGPV1AY4H52N6HECABFZ8WDUGXD5G2",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "TTJKDGPV1AY4H52N6HECABFZ8WDUGXD5G2",
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+    },
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+    },
+    mainnet: {
+      url: process.env.ETHEREUM_RPC,
+      accounts,
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC,
+      accounts,
+    },
+    polygonMumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC,
+      accounts,
+    },
+    arbitrumOne: {
+      url: process.env.ARBITRUM_ONE_RPC,
+      accounts,
+    },
+  },
 };
 
 export default config;
+
+
+
+
--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -8,11 +8,11 @@ const LockModule = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const lock = m.contract("Lock", [unlockTime], { value: lockedAmount });
 
   return { lock };
 });
 
 export default LockModule;
+
+
